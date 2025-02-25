55e223e4b2a6ddc5ec13df9e866b60be14653a30
tsukaharuki <tt23htakaoka@ec.usp.ac.jp>
2025-02-25 16:27:06 +0900

    cp3 completed

--- Tutorial/chapter3.py
+++ Tutorial/chapter3.py
@@ -1,16 +1,33 @@
 import cv2
 
-import numpy as np
 
-img = cv2.imread("Resources/book.jpg")
+#image_path = "IMD-internship-Homework/Tutorial/Resources/book.jpg"
+#print(os.getcwd())
 
+#if image_path is not os.path.exists(image_path):
+    #print("error1")
+
+path = r"Resources/book.jpg"
+img = cv2.imread(path)
+
+if img is None:
+    print("error")
 #画像のサイズを取得
 #h,w = img.shape[:2]
 
 #画像のサイズを2倍にする
-result = cv2.resize(img,None,fx=2,fy=2)
+else:
+    print("OK")
+    result = cv2.resize(img,(300,200))#画像のサイズを幅300,高さ200にした。
+    #resultが存在していなければ"error"を出す。
+    if result is None:
+        print("error")
 
-cv2.imread("Image",result)
+    else:
+        print("OK")
+        cv2.imshow("Image",result)
+        cv2.waitKey(5)
+        cv2.waitKey(0)
+        #cv2.destroyAllwindows()
 
-#cv2.waitKey(0)
 
