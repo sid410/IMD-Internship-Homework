a35797db80abf1eae16ea603443bf3e04b36a7fc
tsukaharuki <tt23htakaoka@ec.usp.ac.jp>
2025-02-25 17:29:41 +0900

    cp6 completed

--- Tutorial/chapter5.py
+++ Tutorial/chapter5.py
@@ -4,7 +4,8 @@ img = cv2.imread(name)
 print(img.shape)
 
 #画像に文字を描画
-cv2.putText(img,"This car is luxury.",(0,30),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255),cv2.LINE_4)
+cv2.putText(img,"This car is luxury.",(0,30),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),cv2.LINE_4)
+cv2.putText(img,"It's very costly but superb.",(180,420),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255),cv2.LINE_4)
 
 
 if img is None:
--- Tutorial/chapter6.py
+++ Tutorial/chapter6.py
@@ -1,1 +1,17 @@
-print("6")
+import cv2
+import numpy as np
+
+path = "Resources/cards.jpg"
+img = cv2.imread(path)
+
+#幅50~100,高さ100~400までの部分を切り取る。
+roi = img[50:200,100:400]
+
+cv2.imshow("kirinui",roi)
+cv2.waitKey(0)
+
+
+
+
+
+
