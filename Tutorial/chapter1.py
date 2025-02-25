4c27e8b39dbb9022c5f63b6be24627a39a72f3cc
tsukaharuki <tt23htakaoka@ec.usp.ac.jp>
2025-02-25 14:41:04 +0900

    cp1 completed

--- Tutorial/chapter1.py
+++ Tutorial/chapter1.py
@@ -1,7 +1,12 @@
 import cv2
 
+
 img = cv2.imread("Resources/1.jpg")
 
-cv2.imshow("Keywords",img)
+#画像をグレー化する
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# cv2.waitkey(0)
+#灰色化した画像を表示
+cv2.imshow("Gray Image",grayscale)
+
+cv2.waitKey(0)
