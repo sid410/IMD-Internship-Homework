9eeeea549636ee0375140ff098557e7dab801e5d
tsukaharuki <tt23htakaoka@ec.usp.ac.jp>
2025-02-25 14:53:10 +0900

    cp2 completed

--- Tutorial/chapter2.py
+++ Tutorial/chapter2.py
@@ -1,1 +1,11 @@
-print("2")
+import cv2
+print("hello")
+img = cv2.imread("Resources/book.jpg")
+
+color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+cv2.imshow("Image",img)
+cv2.imshow("color",color)
+
+
+cv2.waitKey(0)
