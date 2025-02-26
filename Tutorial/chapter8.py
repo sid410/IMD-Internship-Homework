1c32936ecf7a176ed8ec4af22e73445599b2e678
tsukaharuki <tt23htakaoka@ec.usp.ac.jp>
2025-02-26 11:11:45 +0900

    cp8 completed

--- Tutorial/chapter8.py
+++ Tutorial/chapter8.py
@@ -1,1 +1,28 @@
-print("8")
+import cv2
+
+#学習モデル(顔検出器のロード)
+imgface = cv2.CascadeClassifier("Resources/haarcascade_frontalface_default.xml")
+
+img = cv2.imread(r"Resources/lena.png")
+
+if img is None:
+    print("error")
+else:
+    print("yes")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#顔検出
+faces = imgface.detectMultiScale(gray,1.1,8)
+
+#検出した顔を描画
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllwindows()
+
+
+
+
+
--- Tutorial/chapter9.py
+++ Tutorial/chapter9.py
@@ -1,1 +1,3 @@
-print("9")
+import cv2
+
+
