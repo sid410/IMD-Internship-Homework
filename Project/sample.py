98946e9863d7df2efdc781216689df1b95306729
Rmatsuoka0518 <ryosuke518371@gmail.com>
2025-03-05 17:00:21 +0900

    try to extract text

--- Project/sample.py
+++ Project/sample.py
@@ -18,7 +18,7 @@ tools = pyocr.get_available_tools()
 tool = tools[0]
 
 #OCRの設定 ※tesseract_layout=6が精度には重要。デフォルトは3
-builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+builder = pyocr.builders.TextBuilder(tesseract_layout=3)
 
 img = cv2.imread("Project/cut_en_sample.png")
 kernel = np.ones((5,5),np.uint8)
@@ -26,11 +26,16 @@ kernel = np.ones((5,5),np.uint8)
 ##gray
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ##ぼかし
-# imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgBlur = cv2.GaussianBlur(imgGray,(1,1),0)
 ##輪郭
-imgCanny = cv2.Canny(img,200,200)
+imgCanny = cv2.Canny(imgBlur,200,200)
 ##輪郭膨張
 imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+
+#ブラーなし
+imgCanny_no = cv2.Canny(img,200,200)
+##輪郭膨張
+imgDialation_no = cv2.dilate(imgCanny_no,kernel,iterations=1)
 
 # OpenCVの画像（numpy.ndarray）をPIL.Imageに変換
 imgDialation_pil = Image.fromarray(imgDialation)
@@ -42,6 +47,7 @@ img_con = enhancer.enhance(2.0) #コントラストを上げる
 img_con_np = np.array(img_con)
 
 cv2.imshow("Dialation Image",imgDialation)
+cv2.imshow("Blur_no",imgDialation_no)
 cv2.imshow("Con",img_con_np)
 
 # txt_pyocr = tool.image_to_string(imgDialation_pil , lang='eng', builder=builder)
---
+++ Project/sample_en.py
@@ -0,0 +1,40 @@
+import pyocr
+import os
+import cv2
+import numpy as np
+import sys
+from PIL import Image,ImageEnhance
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
+if len(tools) == 0:
+    print("OCR tool is not found.")
+    sys.exit(1)
+lan = "eng"
+
+#OCRエンジン取得
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+#OCRの設定 ※tesseract_layout=6が精度には重要。デフォルトは3
+builder = pyocr.builders.TextBuilder(tesseract_layout=6)
+
+path="Project/cut_en_sample.png"
+
+# path="Project/sample.png"
+
+img = cv2.imread(path)
+imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+imgBlur = cv2.GaussianBlur(imgHSV,(9,9),0)
+lower=np.array([0,0,190])
+upper=np.array([255,255,255])
+mask=cv2.inRange(imgBlur,lower,upper)
+
+cv2.imshow("mask",mask)
+cv2.waitKey(0)
+
+mask_pil = Image.fromarray(mask)
+
+txt_pyocr = tool.image_to_string(mask_pil , lang='eng', builder=builder)
+
+print(txt_pyocr)
