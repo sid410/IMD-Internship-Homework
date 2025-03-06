53b1c8add5cf5cdb6083e1a6e81456d0684d2d3b
Rmatsuoka0518 <ryosuke518371@gmail.com>
2025-03-06 09:58:53 +0900

    Try to  rise the accracy

--- Project/sample.py
+++ Project/sample.py
@@ -5,6 +5,35 @@ import numpy as np
 import sys
 from PIL import Image,ImageEnhance
 
+def detect(img):
+    crop_size = img.shape[0] * img.shape[1]
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    _, img_th = cv2.threshold(img_gray, 190, 250, cv2.THRESH_OTSU)
+
+    nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(img_th)
+
+    if nlabels > 1:
+        median = np.median(stats[:,4])
+       
+        for idx in range(1, nlabels):
+
+            if( stats[idx][4] >= median * 2 ): # not average size
+                img_th[labels == idx, ] = 0
+
+            elif( stats[idx][4] < 5 ): # too small
+                img_th[labels == idx, ] = 0
+
+            elif( (stats[idx][4] / crop_size) > 0.25): # too big
+                img_th[labels == idx, ] = 0
+
+            else:
+                pass
+    img_th_im = Image.fromarray(img_th)
+    text = tool.image_to_string(img_th_im , lang='eng', builder=builder)
+
+    return img_th, text
 
 tools = pyocr.get_available_tools()
 tool = tools[0]
@@ -20,38 +49,49 @@ tool = tools[0]
 #OCRの設定 ※tesseract_layout=6が精度には重要。デフォルトは3
 builder = pyocr.builders.TextBuilder(tesseract_layout=3)
 
-img = cv2.imread("Project/cut_en_sample.png")
-kernel = np.ones((5,5),np.uint8)
+file="Project/cut_en_sample.png"
 
-##gray
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-##ぼかし
-imgBlur = cv2.GaussianBlur(imgGray,(1,1),0)
-##輪郭
-imgCanny = cv2.Canny(imgBlur,200,200)
-##輪郭膨張
-imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
+# img = cv2.imread("Project/cut_en_sample.png")
+# kernel = np.ones((5,5),np.uint8)
 
-#ブラーなし
-imgCanny_no = cv2.Canny(img,200,200)
-##輪郭膨張
-imgDialation_no = cv2.dilate(imgCanny_no,kernel,iterations=1)
+# ##gray
+# imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# ##ぼかし
+# imgBlur = cv2.GaussianBlur(imgGray,(1,1),0)
+# ##輪郭
+# imgCanny = cv2.Canny(imgBlur,200,200)
+# ##輪郭膨張
+# imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
 
-# OpenCVの画像（numpy.ndarray）をPIL.Imageに変換
-imgDialation_pil = Image.fromarray(imgDialation)
+# #ブラーなし
+# imgCanny_no = cv2.Canny(img,200,200)
+# ##輪郭膨張
+# imgDialation_no = cv2.dilate(imgCanny_no,kernel,iterations=1)
 
-enhancer= ImageEnhance.Contrast(imgDialation_pil) #コントラストを上げる
-img_con = enhancer.enhance(2.0) #コントラストを上げる
+# # OpenCVの画像（numpy.ndarray）をPIL.Imageに変換
+# imgDialation_pil = Image.fromarray(imgDialation)
 
-# PIL.Image を numpy.ndarray に変換
-img_con_np = np.array(img_con)
+# enhancer= ImageEnhance.Contrast(imgDialation_pil) #コントラストを上げる
+# img_con = enhancer.enhance(2.0) #コントラストを上げる
 
-cv2.imshow("Dialation Image",imgDialation)
-cv2.imshow("Blur_no",imgDialation_no)
-cv2.imshow("Con",img_con_np)
+# # PIL.Image を numpy.ndarray に変換
+# img_con_np = np.array(img_con)
+
+# cv2.imshow("Dialation Image",imgDialation)
+# cv2.imshow("Blur_no",imgDialation_no)
+# cv2.imshow("Con",img_con_np)
+
+img = cv2.imread(file)
+
+img_result, text = detect(img)
+
+print('Texto: ', text)
+cv2.imshow('Resultado', img_result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # txt_pyocr = tool.image_to_string(imgDialation_pil , lang='eng', builder=builder)
 
 # print(txt_pyocr)
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
--- Project/sample_en.py
+++ Project/sample_en.py
@@ -23,12 +23,17 @@ path="Project/cut_en_sample.png"
 
 # path="Project/sample.png"
 
+kernel = np.array([[ 0, -0.5,  0],
+                   [-0.5,  3, -0.5],
+                   [ 0, -0.5,  0]])
+
 img = cv2.imread(path)
 imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 imgBlur = cv2.GaussianBlur(imgHSV,(9,9),0)
+imgSharp = cv2.filter2D(imgBlur,-1,kernel)
 lower=np.array([0,0,190])
 upper=np.array([255,255,255])
-mask=cv2.inRange(imgBlur,lower,upper)
+mask=cv2.inRange(imgSharp,lower,upper)
 
 cv2.imshow("mask",mask)
 cv2.waitKey(0)
